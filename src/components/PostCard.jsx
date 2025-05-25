2cf7a179d85a9ced7efadbbcddb1a92ed0f84f84
Carlomadella <madella871@gmail.com>
2025-05-25 19:55:54 +0200

    [FEAT] creato file PostCard e esportato in PostsList

---
+++ src/components/PostCard.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const PostCard = () =>{
+    return(
+        <div className="card">
+            <div className="card-body">
+                <h3>Titolo</h3>
+                <p>Categoria:</p>
+                <p>Contenuto:</p>
+            </div>
+        </div>
+    )
+}
+
+export default PostCard
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,12 +1,14 @@
 import React, { useContext } from "react";
 import PostsContext from "../contexts/PostsContext";
+import PostCard from "./PostCard";
+
 const PostsList = () =>{
     const posts = useContext(PostsContext)
     return(
         <div className="row">
             <div className="col-12">
                 {posts.map((post) => {
-                    console.log(post)
+                    return <PostCard />
                 })}
             </div>
         </div>    
