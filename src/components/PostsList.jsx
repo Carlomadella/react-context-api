7be07ae6baadc1b4e71a7f8027b6de1ade069b94
Carlomadella <madella871@gmail.com>
2025-05-25 19:39:05 +0200

    [UPD] creato ciclo per stampare l'array posts

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -3,7 +3,13 @@ import PostsContext from "../contexts/PostsContext";
 const PostsList = () =>{
     const posts = useContext(PostsContext)
     return(
-        <h2>Elenco dei posts</h2>     
+        <div className="row">
+            <div className="col-12">
+                {posts.map((post) => {
+                    console.log(post)
+                })}
+            </div>
+        </div>    
     )
 }
 
