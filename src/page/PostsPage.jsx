f39cc7309a6866f2660b990e4b546cecbb7e8b79
Carlomadella <madella871@gmail.com>
2025-05-25 19:16:41 +0200

    [FEAT] creato file PostsPage

---
+++ src/page/PostsPage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const PostsPage = () =>{
+    return(
+        <div className="container">
+            <div className="row gy-3">
+                <div className="col-12">
+                    <h1>I miei Posts</h1>
+                </div>
+                <div className="col-12">
+                    {}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default PostsPage;
